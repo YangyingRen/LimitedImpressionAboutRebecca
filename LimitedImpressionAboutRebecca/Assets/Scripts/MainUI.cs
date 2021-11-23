8d9a8b042cb58dc10d66aed070de3cc771d72c26
Yangying <ryysophie@126.com>
2021-11-23 18:05:47 -0500

    Adjust UI animation.

--- LimitedImpressionAboutRebecca/Assets/Scripts/MainUI.cs
+++ LimitedImpressionAboutRebecca/Assets/Scripts/MainUI.cs
@@ -44,4 +44,6 @@ public class MainUI : MonoBehaviour
             c=0;
         }
     }
+
+    
 }
--- LimitedImpressionAboutRebecca/Assets/Scripts/Talking.cs
+++ LimitedImpressionAboutRebecca/Assets/Scripts/Talking.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Talking : MonoBehaviour
-{
+{public GameObject SayDialogue;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,12 @@ public class Talking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GameObject.Find("Player").GetComponent<MoveWASD>().enabled=false;
+        if(SayDialogue.active==true){
+            GetComponent<MoveWASD>().enabled=false;
+        }
+        else{
+            GetComponent<MoveWASD>().enabled=true;
+        }
 
     }
 }
