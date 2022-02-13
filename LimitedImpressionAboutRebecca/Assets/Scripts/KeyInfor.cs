1974c471f763288e73e7b2883ee234885b2487a4
Yangying <ryysophie@126.com>
2022-02-12 23:46:04 -0500

    Update UI and Add a complete round

---
+++ LimitedImpressionAboutRebecca/Assets/Scripts/KeyInfor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Fungus;
+
+public class KeyInfor : MonoBehaviour
+{
+    private int KeyInformation, KeyAmount;
+    public Flowchart flowchart;
+    private Image image;
+    public float minimum;
+    public GameObject KeyComplete;
+    // Start is called before the first frame update
+    void Start()
+    {
+    image=GetComponent<Image>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {minimum=image.fillAmount;
+    KeyInformation=flowchart.GetIntegerVariable("KeyInfo");
+    KeyAmount=flowchart.GetIntegerVariable("KeyAmount");
+    image.fillAmount=Mathf.Lerp(minimum,1-((float)KeyInformation/(float)KeyAmount),Time.deltaTime);   
+    if(minimum>0.99){
+        KeyComplete.SetActive(true);
+        KeyComplete.GetComponent<Text>().text="KEY INFORMATION COMPLETED";
+    }  
+    else{
+        KeyComplete.SetActive(false);
+    }
+    }
+}
