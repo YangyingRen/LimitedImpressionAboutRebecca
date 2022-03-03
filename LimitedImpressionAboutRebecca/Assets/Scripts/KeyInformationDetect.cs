b1123625d8750cba329c8d57aa73a37119f30023
Yangying <ryysophie@126.com>
2022-03-03 01:38:24 -0500

    Update Transcript

--- LimitedImpressionAboutRebecca/Assets/Scripts/KeyInformationDetect.cs
+++ LimitedImpressionAboutRebecca/Assets/Scripts/KeyInformationDetect.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
 public class KeyInformationDetect : MonoBehaviour
 {
+    public Flowchart flowchart;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,11 @@ public class KeyInformationDetect : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerExit(Collider col){
+    if(col.tag=="Information"){
+        flowchart.SetIntegerVariable("Information",0);
+    }
+
     }
 }
